fc3b1c07f5ca33e2805e113dcbcebfe4e93ccddf
Brian Whitton <me@brianwhitton.com>
2017-11-13 16:59:52 -0500

    import normalize

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,5 +21,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('node_modules/normalize.css/normalize.css');
   return app.toTree();
 };
