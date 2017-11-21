3dd282fb2cf13de91abd2458f4c7eb30b0254360
Brian Whitton <me@brianwhitton.com>
2017-11-21 11:21:03 -0500

    adds integration test for module-embed

--- tests/integration/components/module-embed/component-test.js
+++ tests/integration/components/module-embed/component-test.js
@@ -1,6 +1,7 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { moduleForComponent } from 'ember-qunit';
+import test from 'ember-sinon-qunit/test-support/test';
 import hbs from 'htmlbars-inline-precompile';
-import { find } from 'ember-native-dom-helpers';
+import { find, fillIn, click } from 'ember-native-dom-helpers';
 
 moduleForComponent('module-embed', 'Integration | Component | module embed', {
   integration: true
@@ -22,4 +23,47 @@ test('it renders', function(assert) {
   `);
 
   assert.ok(find('.module-embed'), 'it renders');
+});
+
+test('embeds and communicates with iframes', function(assert) {
+  let done = assert.async();
+  const PARAMS = {};
+  const NAME = 'foo-name';
+  const SRC = 'foo-src';
+  const KEY = 'test-key'
+  let mockClient = {
+    set: this.mock().once().withArgs(`targetOriginMap.${NAME}`, SRC),
+    addTarget: this.mock().once().withArgs(NAME),
+    fetch: this.mock().once().withArgs(`${NAME}:update`, { [KEY]: 'foo' })
+  };
+  this.setProperties({
+    name: NAME,
+    src: SRC,
+    client: mockClient,
+    params: PARAMS,
+    key: KEY
+  });
+  
+  this.render(hbs`
+    {{#module-embed client=client params=params as |embed|}}
+      {{embed.iframe src=src name=name}}
+      
+      {{labelled-input key=key onInput=embed.sendMessage}}
+      
+      {{embed.generateButton}}
+    {{/module-embed}}
+  `);
+  
+  fillIn('input', 'foo').then(() => {
+    assert.equal(PARAMS['test-key'], encodeURIComponent('foo'));
+    done();
+    
+    click('button');
+    let modal = find('.module-embed__markup');
+    const EMBED = `
+      <div data-pym-src="${SRC}?${KEY}=foo">Loading...</div>
+      <script type="text/javascript" src="https://pym.nprapps.org/pym.v1.min.js"></script>
+    `;
+    assert.equal(modal.textContent.trim(), EMBED.trim(), 'generates expected embed code');
+  });
 });
