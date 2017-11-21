e6f2bf95c05894c8380e075c6632aba9bdb9683d
Brian Whitton <me@brianwhitton.com>
2017-11-21 14:52:33 -0500

    adds copy block component

---
+++ app/components/copy-block/component.js
@@ -0,0 +1,44 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNames: ['copy-block'],
+  
+  didInsertElement() {
+    let input = document.getElementById('copy-paste');
+    this.$(input).height(0).height(input.scrollHeight);
+  },
+  
+  clearSelection() {
+    if (document.selection) {
+      document.selection.empty();
+    } else if (window.getSelection) {
+      window.getSelection().removeAllRanges();
+    }
+  },
+  
+  actions: {
+    copy() {
+      let input = document.getElementById('copy-paste');
+      input.select();
+      let success = document.execCommand('copy');
+      if (success) {
+        this.set('copied', true);
+        this.clearSelection();
+      } else {
+        input.readOnly = false;
+        let range = document.createRange();
+        range.selectNodeContents(input);
+        let selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        input.setSelection(0, 99999);
+        input.readOnly = true;
+        let success = document.execCommand('copy');
+        input.blur();
+        if (success) {
+          this.set('copied', true);
+        }
+      }
+    }
+  }
+});
