4ce03eb61806ad6251196ab7e902ad2ceeec2881
Brian Whitton <me@brianwhitton.com>
2017-11-21 12:49:03 -0500

    give messaging frame a child element for pym to target

--- app/components/messaging-frame/component.js
+++ app/components/messaging-frame/component.js
@@ -1,15 +1,21 @@
 import Component from '@ember/component';
 import { get } from '@ember/object';
+import { computed } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
 import pym from 'pym';
 
 export default Component.extend({
   classNames:        ['messaging-frame'],
   
+  targetId: computed('src', function() {
+    return guidFor(this.get('src'));
+  }),
+  
   didInsertElement() {
     let src = get(this, 'src');
     let register = get(this, 'register');
     if (src) {
-      let parent = new pym.Parent(this.elementId, src);
+      let parent = new pym.Parent(this.get('targetId'), src);
       if (register) {
         register(get(this, 'name'), src, parent.iframe);
       }
