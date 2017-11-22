727ee77640e1713d2d97899a81114884fce4ad7d
Brian Whitton <me@brianwhitton.com>
2017-11-22 11:27:41 -0500

    update module-embed to initialize a new changeset
    validate before saving
    yield it out to context

--- app/components/module-embed/component.js
+++ app/components/module-embed/component.js
@@ -1,5 +1,7 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import lookupValidator from 'ember-changeset-validations';
+import Changeset from 'ember-changeset';
 
 export default Component.extend({
   tagName: 'section',
@@ -8,6 +10,12 @@ export default Component.extend({
   params: {},
   
   client: service('window-messenger-client'),
+  
+  init() {
+    this._super(...arguments);
+    let { params, validations } = this.getProperties('params', 'validations');
+    this.set('changeset', new Changeset(params, lookupValidator(validations), validations, { skipValidate: true }));
+  },
   
   actions: {
     register(name, src, element) {
@@ -18,15 +26,21 @@ export default Component.extend({
     },
     
     postMessage(key, value) {
-      this.set(`params.${key}`, encodeURIComponent(value));
       this.get('client').fetch(`${this.get('name')}:update`, { [key]: value });
     },
     
     generate() {
-      let params = this.get('params');
-      let queryParams = Object.keys(params).map(key => `${key}=${params[key]}`).join('&');
-      this.set('embedCode', `${this.get('src')}${queryParams ? `?${queryParams}` : ''}`);
-      this.set('showDialog', true);
+      let changeset = this.get('changeset');
+      changeset.validate().then(() => {
+        if (changeset.get('isValid')) {
+          changeset.save().then(() => {
+            let params = this.get('params');
+            let queryParams = Object.keys(params).map(key => `${key}=${encodeURIComponent(params[key])}`).join('&');
+            this.set('embedCode', `${this.get('src')}${queryParams ? `?${queryParams}` : ''}`);
+            this.set('showDialog', true);
+          });
+        }
+      });
     },
   }
 });
