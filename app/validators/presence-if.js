2e5ab05ccfa688ec55fd31689243d4520273f5c7
Brian Whitton <me@brianwhitton.com>
2017-11-22 11:32:50 -0500

    look at `changes` instead of content

--- app/validators/presence-if.js
+++ app/validators/presence-if.js
@@ -1,8 +1,8 @@
 import buildMessage from 'ember-changeset-validations/utils/validation-errors';
 
 export default function validatePresenceIf({ otherField, otherFieldLabel } = {}) {
-  return (key, newValue, oldValue, changes, content) => {
-    if (content[otherField]) {
+  return (key, newValue, oldValue, changes/*, content*/) => {
+    if (changes[otherField]) {
       return !!newValue || buildMessage(key, {type: 'presenceIf', context: { otherFieldLabel }});
     } else {
       return true;
