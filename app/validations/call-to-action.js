b76fc18efc3eda54b568548d11261f3dfaab9624
Brian Whitton <me@brianwhitton.com>
2017-11-22 14:41:00 -0500

    Specify summary validation message
    Use human friendly “Short blurb”

--- app/validations/call-to-action.js
+++ app/validations/call-to-action.js
@@ -6,7 +6,7 @@ import validatePresenceIf from '../validators/presence-if';
 
 export default {
   headline: validatePresence(true),
-  summary: validatePresence(true),
+  summary: validatePresence({presence: true, message: "Short blurb can't be blank"}),
   callToAction: validatePresenceIf({otherField: 'url', otherFieldLabel: 'Button URL'}),
   url:[
     validateFormat({type: 'url', allowBlank: true, message: 'URL must be a valid web address.'}),
