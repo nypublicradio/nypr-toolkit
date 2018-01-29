78db093482cd68e8b42fa094720ee9a420fea8db
Brian Whitton <me@brianwhitton.com>
2018-01-29 17:34:10 -0500

    dummy tests for site-header

--- tests/integration/components/site-header/link/component-test.js
+++ tests/integration/components/site-header/link/component-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { find } from 'ember-native-dom-helpers';
 
 moduleForComponent('site-header/link', 'Integration | Component | site header/link', {
   integration: true
@@ -11,14 +12,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{site-header/link}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#site-header/link}}
-      template block text
-    {{/site-header/link}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.ok(find('.site-link'));
 });
