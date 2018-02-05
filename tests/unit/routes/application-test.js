4f0693fbb93811d43ef105be10d6188ee5f518ae
Brian Whitton <me@brianwhitton.com>
2018-02-05 18:53:23 -0500

    adds failing unit test for application route

---
+++ tests/unit/routes/application-test.js
@@ -0,0 +1,22 @@
+import { moduleFor } from 'ember-qunit';
+import test from 'ember-sinon-qunit/test-support/test';
+
+moduleFor('route:application', 'Unit | Route | application', {
+  // Specify the other units that are required for this test.
+  needs: ['service:session']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
+
+test('it calls staffAuth in the beforeModel hook', function() {
+  let route = this.subject({
+    session: {
+      staffAuth: this.mock('staffAuth').once()
+    }
+  });
+
+  route.beforeModel();
+})
