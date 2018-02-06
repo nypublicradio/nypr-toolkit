d962f25a59eb8ef82b654fd32154e4e4c24df607
Brian Whitton <me@brianwhitton.com>
2018-02-06 12:31:11 -0500

    update create route with embed data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,7 @@
 import Route from '@ember/routing/route';
-import config from '../config/environment';
-import fetch from 'fetch';
-
-let { embeds, themesIndex } = config;
 
 export default Route.extend({
   redirect() {
     this.transitionTo('get-started');
-  },
-  model() {
-    return fetch(themesIndex)
-      .then(r => r.json()).then(themes => ({themes, embeds}));
   }
 });
