a9aa791c5f0943bf3d73aa35d3112a0b0915cfd2
Brian Whitton <me@brianwhitton.com>
2018-02-06 12:31:11 -0500

    update create route with embed data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,7 @@
 import Route from '@ember/routing/route';
-import config from '../config/environment';
 
 export default Route.extend({
   redirect() {
     this.transitionTo('get-started');
-  },
-  model() {
-    return config.embeds;
   }
 });
