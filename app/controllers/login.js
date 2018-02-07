a6d5f2c43db48c83f8450e0e5818450cf5157445
Brian Whitton <me@brianwhitton.com>
2018-02-07 10:41:08 -0500

    adds login controller and route

---
+++ app/controllers/login.js
@@ -0,0 +1,28 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import config from 'nypr-toolkit/config/environment';
+import fetch from 'fetch';
+
+export default Controller.extend({
+  session: service(),
+
+  actions: {
+    authenticate(username, password) {
+      // legacy endpoints require form encoding
+      let body = `username=${username}&password=${password}`;
+      fetch(`${config.adminRoot}/api/v1/accounts/login/`, {
+        body,
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(r => r.json())
+        .then(({ success }) => {
+          if (success) {
+            this.transitionToRoute('get-started');
+          }
+        });
+    }
+  }
+});
