7c5508ba9845879d4c16a566f5304a3f78046c59
Brian Whitton <me@brianwhitton.com>
2018-02-16 13:53:39 -0500

    wire up validations for story url inputs

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -1,24 +1,32 @@
 import Controller from '@ember/controller';
 import CTAValidations from '../validations/call-to-action';
 import NewsletterValidations from '../validations/newsletter';
+import PlaylistValidations from '../validations/playlist';
 import URL from 'url-parse';
 
 export default Controller.extend({
   CTAValidations,
   NewsletterValidations,
+  PlaylistValidations,
 
-  actions: {
-    transformStoryURLs(value) {
-      if (value.startsWith('http')) {
-        if (value.endsWith('/')) {
-          value = value.slice(0, -1);
-        }
-        let url = new URL(value);
-        let [slug] = url.pathname.split('/').slice(-1);
-        return slug;
-      } else {
-        return value;
+  transformStoryURLs(value) {
+    if (value.startsWith('http')) {
+      if (value.endsWith('/')) {
+        value = value.slice(0, -1);
       }
+      let url = new URL(value);
+      let [slug] = url.pathname.split('/').slice(-1);
+      return slug;
+    } else {
+      return value;
+    }
+  },
+
+  validateStoryItem({ newValue }) {
+    if (newValue.startsWith('/')) {
+      return ['Please enter a story slug or an absolute url'];
+    } else {
+      return true;
     }
   }
 });
