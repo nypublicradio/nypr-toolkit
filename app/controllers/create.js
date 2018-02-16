ee4269c70b971267234f10367f7dc937aada902f
Brian Whitton <me@brianwhitton.com>
2018-02-16 12:13:57 -0500

    pass in a transform function to the list of story url inputs

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -1,8 +1,24 @@
 import Controller from '@ember/controller';
 import CTAValidations from '../validations/call-to-action';
 import NewsletterValidations from '../validations/newsletter';
+import URL from 'url-parse';
 
 export default Controller.extend({
   CTAValidations,
   NewsletterValidations,
+
+  actions: {
+    transformStoryURLs(value) {
+      if (value.startsWith('http')) {
+        if (value.endsWith('/')) {
+          value = value.slice(0, -1);
+        }
+        let url = new URL(value);
+        let [slug] = url.pathname.split('/').slice(-1);
+        return slug;
+      } else {
+        return value;
+      }
+    }
+  }
 });
