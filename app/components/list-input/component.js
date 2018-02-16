db295f5ad93fca6ca527dbb929db4d05bf17851c
Brian Whitton <me@brianwhitton.com>
2018-02-16 13:52:48 -0500

    support custom validator on yielded inputs inside a list input

--- app/components/list-input/component.js
+++ app/components/list-input/component.js
@@ -1,28 +1,28 @@
 import Component from '@ember/component';
 import { get, set } from '@ember/object';
+import Changeset from 'ember-changeset';
 
 export default Component.extend({
   classNames: ['list-input'],
 
   inputs: 1,
-  values: null,
   key: '',
 
   init() {
     this._super(...arguments);
-    set(this, 'values', []);
+    let validator = this.get('inputValidation');
+    let changeset = new Changeset({}, validator);
+    set(this, 'listChangeset', changeset);
   },
   add() {
     this.incrementProperty('inputs');
   },
   combineValues(index, _, value, transform) {
+    let listChangeset = get(this, 'listChangeset');
+    let values = listChangeset.get('changes').map(({value}) => value);
     let key     = get(this, 'key');
     let onInput = get(this, 'onInput');
-    let values = get(this, 'values');
     let changeset = get(this, 'changeset');
-
-    values[index] = value;
-    set(this, 'values', values);
 
     if (transform) {
       values = values.map(transform);
