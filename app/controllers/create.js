2ed624a23adf61d86003091903cc75bde9aa8136
Brian Whitton <me@brianwhitton.com>
2018-02-18 11:01:34 -0500

    move playlist field validations into validator module

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -1,13 +1,14 @@
 import Controller from '@ember/controller';
 import CTAValidations from '../validations/call-to-action';
 import NewsletterValidations from '../validations/newsletter';
-import PlaylistValidations from '../validations/playlist';
+import PlaylistValidations, { slugOrUrl } from '../validations/playlist';
 import URL from 'url-parse';
 
 export default Controller.extend({
   CTAValidations,
   NewsletterValidations,
   PlaylistValidations,
+  slugOrUrl,
 
   transformStoryURLs(value) {
     if (value.startsWith('http')) {
@@ -19,18 +20,6 @@ export default Controller.extend({
       return slug;
     } else {
       return value;
-    }
-  },
-
-  validateStoryItem({ newValue }) {
-    let message = 'Please enter a story slug or an absolute url';
-    let isURL = newValue.startsWith('http');
-    let isSlug = newValue.match(/[a-z0-9-]+/);
-
-    if (!isSlug && !isURL) {
-      return [message];
-    } else if (isSlug || isURL) {
-      return true;
     }
   }
 });
--- app/validations/playlist.js
+++ app/validations/playlist.js
@@ -3,6 +3,7 @@ import {
   validateLength
 } from 'ember-changeset-validations/validators';
 import commaListLength from '../validators/comma-list-length';
+import validateFormat from 'ember-validators/format';
 
 export default {
   title: validatePresence({presence: true}),
@@ -12,3 +13,18 @@ export default {
     validatePresence({presence: true})
   ]
 };
+
+export function slugOrUrl({ newValue }) {
+  const MESSAGE = 'Please enter a story slug or an absolute url';
+  let urlValidation = validateFormat(newValue, {type: 'url', allowBlank: true});
+  let slugValidation = validateFormat(newValue, {regex: /^[a-z\d-_]+$/, allowBlank: true});
+
+  let isURL = urlValidation === true;
+  let isSlug = slugValidation === true;
+
+  if (!isSlug && !isURL) {
+    return [MESSAGE];
+  } else {
+    return true;
+  }
+}
