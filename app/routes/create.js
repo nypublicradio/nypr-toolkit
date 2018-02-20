9c30c4c4c14b2b3592350b7618c164494d7fa3b9
Brian Whitton <me@brianwhitton.com>
2018-02-20 15:03:44 -0500

    pull in mailchimp lists on create route

--- app/routes/create.js
+++ app/routes/create.js
@@ -1,12 +1,22 @@
 import Route from '@ember/routing/route';
 import config from '../config/environment';
 import fetch from 'fetch';
+import RSVP from 'rsvp';
 
-let { embeds, themesIndex } = config;
+let { embeds, themesIndex, mailchimpProxy } = config;
 
 export default Route.extend({
   model() {
-    return fetch(themesIndex)
-      .then(r => r.json()).then(themes => ({themes, embeds}));
+    let themes = fetch(themesIndex)
+      .then(r => r.json());
+    let mailchimpLists = fetch(`${mailchimpProxy}/lists?fields=lists.name,lists.id&count=200`)
+      .then(r => r.json())
+      .then(({lists}) => lists.map(({name, id}) => ({label: name, value: id})));
+
+    return RSVP.hash({
+      themes,
+      mailchimpLists,
+      embeds: RSVP.Promise.resolve(embeds)
+    });
   }
 });
