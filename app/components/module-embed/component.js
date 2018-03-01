fbbbe45a214d4e6bc2b0ce0ffaf66831df816516
Brian Whitton <me@brianwhitton.com>
2018-03-01 14:13:40 -0500

    bring these naming conventions into line

--- app/components/module-embed/component.js
+++ app/components/module-embed/component.js
@@ -10,7 +10,8 @@ export default Component.extend({
   tagName: 'section',
   classNames: ['module-embed'],
 
-  queue: null,
+  pendingRegistrations: null,
+  pendingSubscriptions: null,
   // if params are passed in, use those
   // otherwise set a new blank object in init so state isn't shared
   params: null,
@@ -24,21 +25,21 @@ export default Component.extend({
     let { validations, params } = this.getProperties('params', 'validations');
     this.set('changeset', new Changeset(params, lookupValidator(validations), validations, { skipValidate: true }));
 
-    this.set('queue', []);
+    this.set('pendingRegistrations', []);
     this.set('pendingSubscriptions', []);
   },
 
-  flushQueue() {
+  flushRegistrations() {
     if (this.get('registered')) {
-      let flushed = this.get('queue').reduce((flush, datum) => assign(flush, datum), {});
+      let flushed = this.get('pendingRegistrations').reduce((flush, datum) => assign(flush, datum), {});
       this.get('frame').sendMessage('incoming', JSON.stringify(flushed));
-      this.set('queue', []);
+      this.set('pendingRegistrations', []);
     } else {
-      later(this, 'flushQueue', 50);
+      later(this, 'flushRegistrations', 50);
     }
   },
 
-  subscribeAll(frame) {
+  flushSubscriptions(frame) {
     let subs = this.get('pendingSubscriptions');
     this.set('pendingSubscriptions', []);
 
@@ -58,14 +59,14 @@ export default Component.extend({
       this.set('frame', frame);
 
       if (this.get('pendingSubscriptions.length')) {
-        this.subscribeAll(frame);
+        this.flushSubscriptions(frame);
       }
     },
 
     postMessage(key, value) {
       let data = {[key]: value};
-      this.get('queue').pushObject(data);
-      this.flushQueue();
+      this.get('pendingRegistrations').pushObject(data);
+      this.flushRegistrations();
     },
 
     generate() {
