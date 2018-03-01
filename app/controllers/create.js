b5f1622b6934d539cb65248d1da67b30da99baae
Brian Whitton <me@brianwhitton.com>
2018-03-01 14:14:06 -0500

    move these subscriptions into playlist validations
    because really they're validations based on a response from the iframe

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -1,7 +1,7 @@
 import Controller from '@ember/controller';
 import CTAValidations from '../validations/call-to-action';
 import NewsletterValidations from '../validations/newsletter';
-import PlaylistValidations, { slugOrUrl } from '../validations/playlist';
+import PlaylistValidations, { slugOrUrl, storyNotFound } from '../validations/playlist';
 import URL from 'url-parse';
 
 export default Controller.extend({
@@ -9,23 +9,7 @@ export default Controller.extend({
   NewsletterValidations,
   PlaylistValidations,
   slugOrUrl,
-
-  init() {
-    this._super(...arguments);
-    this.set('playlistSubscriptions', {
-      stories: [{
-        message: 'not-found',
-        callback(component, slug) {
-          let changeset = component.get('listChangeset');
-          let change = changeset.get('changes').find(({ value }) => value.split('/').filter(s => s).slice(-1)[0] === slug);
-          if (change) {
-            let { key } = change;
-            changeset.addError(key, ["This is not a valid story or is not yet published. Double check the story's url."]);
-          }
-        }
-      }]
-    });
-  },
+  playlistSubscriptions: storyNotFound,
 
   transformStoryURLs(value) {
     if (value.startsWith('http')) {
