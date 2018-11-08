df1efbcffb708fcbb4ca090602a512099e8b6c46
walsh9 <walsh9@gmail.com>
2018-11-08 17:03:05 -0500

    Update hidden input module widget to handle dynamic values correctly

--- app/components/hidden-input/component.js
+++ app/components/hidden-input/component.js
@@ -1,5 +1,19 @@
 import LabelledInput from '../labelled-input/component';
+import { get, set, observer } from '@ember/object';
+import { next } from '@ember/runloop';
 
 export default LabelledInput.extend({
   classNames: ['hidden-input'],
+  valueChanged: observer('value', function() {
+    let onChange = this.get('onChange');
+    let changeset = get(this, 'changeset');
+    let key = get(this, 'key');
+    let newValue = get(this, 'value');
+    next(() => {
+      if (onChange) {
+        onChange(key, newValue);
+      }
+      set(changeset, key, newValue);
+    });
+  })
 });
