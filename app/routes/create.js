757561d659fd035d5f962c7b4dbb134c82f40708
Nico Castro <ncastro@nypublicradio.org>
2020-08-14 11:47:04 -0400

    upping the max number of mailchimp lists fetched to 400

--- app/routes/create.js
+++ app/routes/create.js
@@ -9,7 +9,7 @@ export default Route.extend({
   model() {
     let themes = fetch(themesIndex)
       .then(r => r.json());
-    let mailchimpLists = fetch(`${mailchimpProxy}/lists?fields=lists.name,lists.id&count=200`)
+    let mailchimpLists = fetch(`${mailchimpProxy}/lists?fields=lists.name,lists.id&count=400`)
       .then(r => r.json())
       .then(({lists}) => lists.sort((a, b) => a.name.localeCompare(b.name))
                               .map(({name, id}) => ({label: name, value: id})));
